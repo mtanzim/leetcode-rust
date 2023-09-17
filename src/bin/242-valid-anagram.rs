c567565cb811b579c47d2c063fdc156e4807c217
Tanzim Mokammel <mtanzim@gmail.com>
2023-09-17 13:23:49 -0400

    is anagram

--- src/bin/242-valid-anagram.rs
+++ src/bin/242-valid-anagram.rs
@@ -5,9 +5,44 @@
  */
 
 // @lc code=start
+use std::collections::HashMap;
 impl Solution {
     pub fn is_anagram(s: String, t: String) -> bool {
-        false
+        if s.len() != t.len() {
+            return false;
+        }
+        let mut tracker_s = HashMap::new();
+        let mut tracker_t = HashMap::new();
+        for c in s.chars() {
+            let _ = match tracker_s.get(&c) {
+                Some(&found) => tracker_s.insert(c, found + 1),
+                _ => tracker_s.insert(c, 1),
+            };
+        }
+        for c in t.chars() {
+            let _ = match tracker_t.get(&c) {
+                Some(&found) => tracker_t.insert(c, found + 1),
+                _ => tracker_t.insert(c, 1),
+            };
+        }
+
+        for (k, v) in tracker_s {
+            if !tracker_t.contains_key(&k) {
+                return false;
+            }
+            match tracker_t.get(&k) {
+                Some(vt) => {
+                    if v != *vt {
+                        return false;
+                    }
+                }
+                None => {
+                    return false;
+                }
+            }
+        }
+
+        true
     }
 }
 // @lc code=end
@@ -17,4 +52,15 @@ fn main() {
     println!("{}", res)
 }
 
-mod tests_valid_anagram {}
+mod tests_valid_anagram {
+    #[test]
+    fn is_anagram() {
+        let res = super::Solution::is_anagram(String::from("anagram"), String::from("naagram"));
+        assert_eq!(res, true)
+    }
+    #[test]
+    fn is_not_anagram() {
+        let res = super::Solution::is_anagram(String::from("anagramss"), String::from("naagram"));
+        assert_eq!(res, false)
+    }
+}
