8e652af2110a5261fb553de1c085364c611d4046
Tanzim Mokammel <mtanzim@gmail.com>
2023-09-24 19:08:00 -0400

    add some gippity docs for later

--- src/bin/206-reverse-linked-list.rs
+++ src/bin/206-reverse-linked-list.rs
@@ -73,7 +73,33 @@ impl Solution {
         stack.reverse();
         Self::generate_ll(stack)
     }
-    // TODO: understand what is actually happening here with the borrow/move etc etc 🤷
+    /// #### Gippity explains its own `generate_ll` function
+    /// Absolutely, let's dive into the moves and borrows in the given Rust code more clearly.
+    /// 1. **Moves**:
+    /// In Rust, ownership and moves are important concepts for memory safety. When you move a value, you transfer ownership from one variable to another, and the original variable can no longer be used.
+    /// ```rust
+    /// let mut head = None;
+    /// ```
+    /// Here, `None` is moved into `head`. `head` is now the owner of this `Option<Box<ListNode>>` value.
+    /// ```rust
+    /// let new_node = Some(Box::new(ListNode::new(number)));
+    /// ```
+    /// In this line, a new `Option<Box<ListNode>>` is created, and `new_node` takes ownership of this value.
+    /// ```rust
+    /// *cur_node = new_node;
+    /// ```
+    /// The `new_node` is moved into the `cur_node` which is a mutable reference to `head`. `cur_node` takes ownership of the `new_node`, and now `cur_node` is the owner of the `Option<Box<ListNode>>`.
+    /// 2. **Borrows**:
+    /// Borrows in Rust refer to references or temporary permissions to use a value, but they don't own the value. Borrowing helps ensure that while some part of the code is using a value, it cannot be modified or moved elsewhere.
+    /// ```rust
+    /// let mut cur_node = &mut head;
+    /// ```
+    /// Here, a mutable reference `&mut head` is created and assigned to `cur_node`. This means that `cur_node` is temporarily borrowing a mutable reference to the value owned by `head`.
+    /// ```rust
+    /// cur_node = &mut cur_node.as_mut().unwrap().next;
+    /// ```
+    /// This line updates `cur_node` to point to the `.next` of the previous node, effectively "moving" the reference to the next node in the linked list. This is a mutable borrow, meaning we're temporarily modifying where `cur_node` is pointing to.
+    /// Understanding moves and borrows is crucial in Rust to ensure memory safety and prevent common issues like use-after-move or use-after-free. Rust's ownership system enforces strict rules around these concepts to guarantee memory safety at compile time.
     pub fn generate_ll(nums: Vec<i32>) -> Option<Box<ListNode>> {
         let mut head = None;
         let mut cur_node = &mut head;
